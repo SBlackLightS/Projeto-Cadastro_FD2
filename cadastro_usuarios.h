32889b68b938c874543bd3d3b53aef5a7e44c835
Vitor Hugo Leite <vitorhugosa2020@gmail.com>
2022-06-11 18:27:33 -0300

    second commit

---
+++ cadastro_usuarios.h
@@ -0,0 +1,19 @@
+/**
+ * @file CadastroCelulares_1.c
+ * @author Matheus Hirata Vanzela (matheusvanzela@alunos.utfpr.edu.br)
+ *         Vitor Hugo L. A. de Oliveira (vitoroliveira.2021@alunos.utfpr.edu.br)
+ * @brief Projeto de cadastro de marcas e celulares
+ * @version 1.0
+ * @date 2022-06-08
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#ifndef CADASTRO_USUARIOS_H
+#define CADASTRO_USUARIOS_H
+
+
+
+
+#endif
---
+++ interfaces.h
@@ -0,0 +1,21 @@
+/**
+ * @file CadastroCelulares_1.c
+ * @author Matheus Hirata Vanzela (matheusvanzela@alunos.utfpr.edu.br)
+ *         Vitor Hugo L. A. de Oliveira (vitoroliveira.2021@alunos.utfpr.edu.br)
+ * @brief Projeto de cadastro de marcas e celulares
+ * @version 1.0
+ * @date 2022-06-08
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#ifndef INTERFACES_H
+#define INTERFACES_H
+
+/**
+ * @brief limpa todos os caracteres da tela do console
+ */
+void limpaTela ();
+
+#endif
