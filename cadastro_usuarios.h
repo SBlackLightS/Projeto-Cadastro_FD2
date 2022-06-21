4e39ec6a23c3863dfe4808daa6c3cca426716471
Matheus Vanzela <92858958+Matthias-hrT@users.noreply.github.com>
2022-06-21 00:27:36 -0300

    Add files via upload

--- cadastro_usuarios.h
+++ cadastro_usuarios.h
@@ -14,15 +14,23 @@
 #define CADASTRO_USUARIOS_H
 
   /**
-   * @brief Essa funcao verifica se existe o arquivo contendo a quantidade de 
-   * cadastros e os cadastros. Caso nao exista, cria um novo e a quatidade de
-   * usuarios sera 0. Se ja existir, a quantidade de usuarios e atualizada para
-   * o primeiro inteiro salvo no arquivo. 
+   * @brief Essa funcao procura a primeira posicao livre para novo cadastro.
+   * Se nao houver, tenta realocar a quantidade de posicoes para cadastros.
+   * Fecha o programa se nao ocorrer com sucesso.
    * 
+   * @param usuarios 
    * @param quantidade_usuarios 
-   * @return int quantidade de cadastros. Retorna por mera conveniencia.
    */
-  int verifica_arquivo_cadastros (int* quantidade_usuarios);
+  void cadastro_usuarios(Cadastros* usuarios, int* quantidade_usuarios);
+
+  /**
+   * @brief Essa funcao procura espacos vazios alocados para cadastrar novos usuarios
+   * 
+   * @param usuarios 
+   * @param quantidade_usuarios 
+   * @return int (posicao vazia ou -1 para nao existe posicao vazia)
+   */
+  int encontra_posicao_vazia (Cadastros* usuarios, int* quantidade_usuarios);
 
   /**
    * @brief Essa funcao realoca a estrutura que guarda os cadastros dos usuarios
@@ -35,22 +43,14 @@
   Cadastros* realoca_usuarios (Cadastros* usuarios, int* quantidade_usuarios);
 
   /**
-   * @brief Essa funcao procura espacos vazios alocados para cadastrar novos usuarios
+   * @brief Essa funcao verifica se existe o arquivo contendo a quantidade de 
+   * cadastros e os cadastros. Caso nao exista, cria um novo e a quatidade de
+   * usuarios sera 0. Se ja existir, a quantidade de usuarios e atualizada para
+   * o primeiro inteiro salvo no arquivo. 
    * 
-   * @param usuarios 
    * @param quantidade_usuarios 
-   * @return int (posicao vazia ou -1 para nao existe posicao vazia)
+   * @return int quantidade de cadastros. Retorna por mera conveniencia.
    */
-  int encontra_posicao_vazia (Cadastros* usuarios, int* quantidade_usuarios);
-
-  /**
-   * @brief Essa funcao procura a primeira posicao livre para novo cadastro.
-   * Se nao houver, tenta realocar a quantidade de posicoes para cadastros.
-   * Fecha o programa se nao ocorrer com sucesso.
-   * 
-   * @param usuarios 
-   * @param quantidade_usuarios 
-   */
-  void cadastro_usuarios(Cadastros* usuarios, int* quantidade_usuarios);
+  int verifica_arquivo_cadastros (int* quantidade_usuarios);
 
 #endif
